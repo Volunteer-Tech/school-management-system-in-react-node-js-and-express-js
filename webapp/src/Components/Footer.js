74756c5fbfb435fb85d0741df88cd51b2bbd3cf5
Marta Taddese <135144339+martataddese@users.noreply.github.com>
2023-06-20 18:06:48 +0300

    Create Footer.js

---
+++ webapp/src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer ">
+        <div className="float-right">
+          <Link to="/about-us">About </Link>| <Link to="/contact">Contact</Link>
+        </div>
+        <div className="mr-auto">© Volunteer Tech®</div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
